05585101cb3e268b72df71b9e6a224e29ad08f1f
CadenCGS <cserrato21352@columbushs.com>
2020-12-06 21:11:04 -0500

    finished

--- app.js
+++ app.js
@@ -3,29 +3,39 @@
   let rover = {
     direction: "N",
     x: 0,
-    y: 0
+    y: 0,
+    travellog: []
   };
 // ======================
+
+function createBoard(columns, rows) {
+	var matrix = [];
+	for (var i = 0; i < columns; i++) {
+		matrix[i] = new Array(rows);
+	}
+	return matrix;
+}
+
+var board = createBoard(10, 10);
+console.log(board);
+
 function turnLeft(){
   console.log("turnLeft was called!");
   switch (rover.direction) {
     case "N":
       rover.direction = "W";
-      console.log("The rover is now facing " + rover.direction);
       break;
     case "W":
       rover.direction = "W";
-      console.log("The rover is now facing " + rover.direction);
       break;
     case "S":
       rover.direction = "E";
-      console.log("The rover is now facing " + rover.direction);
       break;
     case "E":
       rover.direction = "N";
-      console.log("The rover is now facing " + rover.direction);
       break;
     }
+    rover.travellog.push('Rover turned left and is now facing ' + rover.direction);
 }
 
 function turnRight(){
@@ -33,47 +43,83 @@ function turnRight(){
   switch (rover.direction) {
   case "N":
     rover.direction = "E";
-    console.log("The rover is now facing " + rover.direction);
     break;
   case "E":
     rover.direction = "S";
-    console.log("The rover is now facing " + rover.direction);
     break;
   case "S":
     rover.direction = "W";
-    console.log("The rover is now facing " + rover.direction);
     break;
   case "W":
     rover.direction = "N";
-    console.log("The rover is now facing " + rover.direction);
     break;
   }
+  rover.travellog.push('Rover turned right and is now facing ' + rover.direction);
 }
 
 function moveForward(){
   console.log("moveForward was called")
   switch (rover.direction) {
     case "N":
-      console.log("The rover's was at " + rover.x + ", " + rover.y);
       rover.y--;
-      console.log("The rover's is now " + rover.x + ", " + rover.y);
+      onTheGrid()
       break;
     case "E":
-      console.log("The rover's was at " + rover.x + ", " + rover.y);
       rover.x++;
-      console.log("The rover's is now " + rover.x + ", " + rover.y);
+      onTheGrid()
       break;
     case "S":
-      console.log("The rover's was at " + rover.x + ", " + rover.y);
       rover.y++;
-      console.log("The rover's is now " + rover.x + ", " + rover.y);
+      onTheGrid()
       break;
     case "W":
-      console.log("The rover's was at " + rover.x + ", " + rover.y);
       rover.x--;
-      console.log("The rover's is now " + rover.x + ", " + rover.y);
+      onTheGrid()
       break;
     }
+    rover.travellog.push('Rover moved forward and is now at ' + rover.x + ', ' + rover.y);
+}
+
+function moveBackward(){
+  console.log("moveBackward was called")
+  switch (rover.direction) {
+    case "N":
+      rover.y++;
+      onTheGrid()
+      break;
+    case "E":
+      rover.x--;
+      onTheGrid()
+      break;
+    case "S":
+      rover.y--;
+      onTheGrid()
+      break;
+    case "W":
+      rover.x++;
+      onTheGrid()
+      break;
+    }
+    rover.travellog.push('Rover moved backward and is now at ' + rover.x + ', ' + rover.y);
+}
+
+function onTheGrid(){
+  if (rover.x > 10){
+    rover.x--;
+  }
+
+  if (rover.x < -10){
+    rover.x++;
+  }
+
+  if (rover.y > 10){
+    rover.y--;
+  }
+
+  if (rover.y < -10){
+    rover.y++;
+  }
+  rover.travellog.push('Rover tried to move ' + rover.direction + ' and was stopped by the boundary at ' + rover.x + ', ' + rover.y);
 }
 
 let roverRoute = String("rffrfflfrff");
@@ -84,12 +130,20 @@ function multiCommand(roverRoute){
       turnRight();
     }
 
-    if (roverRoute[i] == "l"){
+    else if (roverRoute[i] == "l"){
       turnLeft();
     }
 
-    if (roverRoute[i] == "f"){
+    else if (roverRoute[i] == "f"){
       moveForward();
+    }
+
+    else if (roverRoute[i] == "b"){
+      moveBackward();
+    }
+
+    else if (roverRoute[i] != "r" || roverRoute[i] != "l" || roverRoute[i] != "f" || roverRoute[i] != "b"){
+      null;
     }
   }
 }
